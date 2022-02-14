b1d33a356700d95c480d475322ceb54fc81da4de
Su-Hyun Park <lovehyun@hotmail.com>
2022-02-14 22:36:47 +0900

    [DEV] fix linting issue. make my code prettier

--- hello.py
+++ hello.py
@@ -1,13 +1,15 @@
 class Hello:
     def say(self):
         print('hello, world')
+
     def add(self, x, y):
-        # print("x+y=",(x+y))
+        print("hahahah hello this is my addition function kkkkkkkkkkkkkkkkkkkkkkkkkkkkk x+y=", (x+y))
         a = x
         b = y
         return (a+b)
 
+
 if __name__ == "__main__":
     h = Hello()
     h.say()
-    print(h.add(2,3))
+    print(h.add(2, 3))
--- test_hello.py
+++ test_hello.py
@@ -1,19 +1,21 @@
 import unittest
 import hello
 
+
 class TestHello(unittest.TestCase):
+
     def test_add_positive(self):
         h = hello.Hello()
-        result = h.add(2,3)
+        result = h.add(2, 3)
         self.assertEqual(result, 5)
-        result = h.add(2,5)
+        result = h.add(2, 5)
         self.assertEqual(result, 7)
 
     def test_add_negative(self):
         h = hello.Hello()
-        result = h.add(-2,3)
+        result = h.add(-2, 3)
         self.assertEqual(result, 1)
-        result = h.add(-2,-5)
+        result = h.add(-2, -5)
         self.assertEqual(result, -7)
 
 
