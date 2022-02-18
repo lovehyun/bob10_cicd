728fe0d412d3d4e35b7f148f6f911b0ac74b358d
Su-Hyun Park <lovehyun@hotmail.com>
2022-02-18 20:12:16 +0900

    [FIX] flake error fix

--- flask/app.py
+++ flask/app.py
@@ -8,9 +8,11 @@ app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1)
 
 color = os.environ.get('APP_COLOR')
 
+
 @app.route('/')
 def main():
     return render_template('hello.html', color=color)
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
